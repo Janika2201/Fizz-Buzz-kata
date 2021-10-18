783d082ccf88afa7996da839e4389dd1b22d8f61
Opilane TTHK <opilane@tthk.ee>
2021-10-18 13:57:03 +0300

    Req1 - get 100 number - pass

---
+++ src/ee/tthk/tarpv19/jv/FizzBuzz.java
@@ -0,0 +1,17 @@
+package ee.tthk.tarpv19.jv;
+
+import java.util.Arrays;
+
+public class FizzBuzz {
+private int[] numbers;
+	
+	public int[] getNumbers() {
+		numbers = new int[100];
+		for(int i = 0; i< numbers.length; i++) {
+			numbers[i]=i+1;
+		}
+		return numbers;
+	}
+}
+
+
