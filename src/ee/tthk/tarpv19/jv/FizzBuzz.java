9241b6c93927ae4d01b6ccd42cf6f6e0818d87a5
Opilane TTHK <opilane@tthk.ee>
2021-10-25 10:49:03 +0300

    Req13- Return zero - pass

--- src/ee/tthk/tarpv19/jv/FizzBuzz.java
+++ src/ee/tthk/tarpv19/jv/FizzBuzz.java
@@ -13,7 +13,10 @@ private int[] numbers;
 		return numbers;
 	}
 	public String fizzbuzz(int value) {
-		 if (value % 3 == 0 || value % 5 == 0) {
+		if(value == 0){
+			return Integer.toString(value);
+		}
+		else if (value % 3 == 0 || value % 5 == 0) {
 			if (value % 3 == 0 && value % 5 == 0) {
 				return "fizzbuzz";
 			} else if (value % 3 == 0) {
